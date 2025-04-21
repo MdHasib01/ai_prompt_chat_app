6341e038641d4e5531d526e5deeb3a8b94c8798a
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-04-22 01:48:01 +0600

    fix the search bar

--- app/components/SearchHandler.tsx
+++ app/components/SearchHandler.tsx
@@ -1,18 +1,18 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useSidebar } from "@/components/ui/sidebar";
+import { useIsMobile, useSidebar } from "@/components/ui/sidebar";
 import React from "react";
 import { BsStars } from "react-icons/bs";
 
 const SearchHandler = () => {
-  const { state } = useSidebar();
+  const { state, isMobile } = useSidebar();
+
   return (
     <div
-      className="flex-col w-full fixed mt-auto bottom-0 bg-white dark:bg-zinc-950 rounded-b-lg z-10"
+      className="flex-col w-full fixed mt-auto bottom-0 px-4 bg-white dark:bg-zinc-950 rounded-b-lg z-10"
       style={{
         scrollbarWidth: "none",
-        width:
-          state === "collapsed" ? "100%" : `calc(100% - var(--sidebar-width))`,
+        width: isMobile ? "100%" : `calc(100% - var(--sidebar-width))`,
       }}
     >
       <div className="mx-auto max-w-4xl relative border rounded-lg my-2">
